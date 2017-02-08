e156798fcfda1867ee6015bcd341280c342cd87f
Julian Thatcher <daedalus.raistlin@gmail.com>
2017-02-08 17:31:43 +1100

    Tentative fix for allowing extra directories

--- genfiles.sh
+++ genfiles.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+#
+# Generate files.js, which will import the JSON AST format of all found
+# Lithp AST's.
+#
+# Additional paths can be supplied.
+
 pushd node_modules/lithp
 if [ ! -e run ]; then
 	ln -s run.js run
@@ -8,21 +14,43 @@ make clean
 # Compile in compact mode
 make RUNFLAGS=-cc
 popd
-files=`find . -name *.ast`
+
 content=$(cat << EOF
 // files.js, generated from genfiles.sh\nvar files = {};
 EOF
 )
 
-for file in $files; do
-	asJson=`echo $file | sed 's/\.ast$/.json/g'`
-	cp $file $asJson
-	proper=`echo $asJson | sed 's/^..node_modules.//g'`
-	properAst=`echo $proper | sed 's/\.json$/.ast/g' | sed 's/^lithp\///g'`
-	content+="\nfiles['$properAst'] = require('$proper');"
+getContents () {
+	set -x
+	path=$1
+	prefix=`echo $path | sed 's/^..\///g'`
+	if [ "$path"x != "x" ]; then
+		pushd $path
+		prefix+="/"
+		path+="/"
+	fi
+	files=`find . -name '*.ast'`
+
+	for file in $files; do
+		asJson=`echo $file | sed 's/\.ast$/.json/g'`
+		cp $file $asJson
+		proper=`echo $asJson | sed 's/^..node_modules.//g' | sed 's/^\.\///g'`
+		properAst=`echo $proper | sed 's/\.json$/.ast/g' | sed 's/^lithp\///g'`
+		content+="\nfiles['$prefix$properAst'] = require('$path$proper');"
+	done
+
+	set +x
+	if [ "$path"x != "x" ]; then
+		popd
+	fi
+}
+
+getContents ""
+for extra in $@; do
+	getContents $@
 done
 
-content+="\nmodule.exports = files;\n"
 
+content+="\nmodule.exports = files;\n"
 echo -e $content > files.js
 
